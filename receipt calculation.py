49b079110280afe4fd9575f24ca687305333b706
denisCabrera <dcab1995@gmail.com>
2021-11-28 22:47:30 -0500

    first commit

--- receipt calculation.py
+++ receipt calculation.py
@@ -4,9 +4,9 @@ prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
-total_price = 0
+total_price = 0  
 for price in prices:
-  total_price = total_price + price
+  total_price = total_price + price               #calculates price
 average_price = total_price / len(prices)
 print("Average Haircut Price: " + str(average_price))
 
